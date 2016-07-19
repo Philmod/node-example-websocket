e65bddb760ff57cfa6977b6e968febf53b425bf9
Philmod <philippe.modard@gmail.com>
2016-07-18 23:40:19 -0400

    redis for socket.io

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm install
 
+ENV REDIS_HOST 192.168.99.100
+ENV REDIS_PORT 6379
+
 EXPOSE 3002
 
 CMD [ "npm", "start"]
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var io = require('socket.io')(3002);
+var redis = require('socket.io-redis');
+
+var adapter = redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+});
+var errFn = function(err) { console.error(err); }
+adapter.pubClient.on('error', errFn);
+adapter.subClient.on('error', errFn);
+
+io.adapter(adapter);
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
