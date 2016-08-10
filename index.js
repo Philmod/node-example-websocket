3ed13de64995da0850687b9c916d31ed6650f717
Philmod <philippe.modard@gmail.com>
2016-08-10 00:54:55 -0400

    morgan http logger

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var os = require("os");
+var morgan = require('morgan');
 var app = require('express')();
 var serverStatus = require('express-server-status');
 
+app.use(morgan(config.logger));
 app.use('/status', serverStatus(app));
 
 var server = require('http').createServer(app);
@@ -20,6 +22,7 @@ adapter.subClient.on('error', errFn);
 io.adapter(adapter);
 
 io.on('connection', function(socket) {
+  console.log('Connection : ', new Date());
   socket.on('chat message', function(content) {
     console.log('Message in : ', content);
     io.emit('chat message', {
