3e39fba63c9751499b39acb780507165ecefc911
Philmod <philippe.modard@gmail.com>
2016-10-10 22:24:36 -0400

    send username and new user

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use('/status', serverStatus(app));
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server, {
-  path: process.env.WEBSOCKET_PATH
+  path: process.env.WEBSOCKET_PATH | 'socket.io'
 });
 
 var redis = require('socket.io-redis');
@@ -24,12 +24,34 @@ adapter.subClient.on('error', errFn);
 io.adapter(adapter);
 
 io.on('connection', function(socket) {
+  var addedUser = false;
+  var hostname = os.hostname();
   console.log('Connection : ', new Date());
-  socket.emit('hostname', os.hostname());
+  socket.emit('hostname', hostname);
 
-  socket.on('chat message', function(content) {
-    console.log('Message in : ', content);
-    io.emit('chat message', content);
+  socket.on('new_message', function(message) {
+    console.log('Message in : ', message);
+    io.emit('new_message', {
+      username: socket.username,
+      message: message,
+    });
+  });
+
+  socket.on('add_user', function(username) {
+    if (addedUser) return;
+    socket.username = username;
+    addedUser = true;
+    io.emit('user_joined', {
+      username: socket.username,
+      hostname: hostname,
+    });
+  });
+
+  socket.on('disconnect', function() {
+    if (!addedUser) return;
+    io.emit('user_left', {
+      username: socket.username
+    });
   });
 });
 
